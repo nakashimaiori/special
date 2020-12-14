dd7e88264d572d7c15c0f5a2c49845d3215e3c22
vagrant <vagrant@localhost.localdomain>
2020-12-14 11:56:39 +0000

    fix_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'user/show'
-  get 'user/index'
-  get 'user/edit'
-  get 'post/show'
-  get 'post/index'
+  resources :users, only: [:index, :show, :edit]
+  resources :posts, only: [:index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
